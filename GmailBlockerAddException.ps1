be8c8e9cc6c0cf435282ccf9a4805e9a77effd9a
rbarbrow <rbarbrow@markonsolutions.com>
2021-09-17 18:12:29 -0400

    testing

--- GmailBlockerAddException.ps1
+++ GmailBlockerAddException.ps1
@@ -1,12 +1,19 @@
 #GmailBlocker
-Install-Module -Name ExchangeOnlineManagement
+#Install-Module -Name ExchangeOnlineManagement
 Import-Module ExchangeOnlineManagement
 Connect-ExchangeOnline
 
+# -1 is unlimited
+#$formatenumerationlimit = -1
+
 Get-TransportRule
 Get-TransportRule "Gmail Block" | Select-Object * | Format-List
-Get-TransportRule "Gmail Block" | Select-Object "ExceptIfFrom" | Format-List | Out-File "c:\z\gmailrule.txt"
+#Get-TransportRule "Gmail Block" | Select-Object "ExceptIfFrom" | Format-List  | Out-File "c:\z\gmailrule.txt"
+$test = Get-TransportRule "Gmail Block" | Select-Object "ExceptIfFrom" | Format-List  
 
+Write-Output $test
+Get-TransportRule "Gmail Block" | Select-Object "ExceptIfFrom" 
+#Set-TransportRule -Identity "Sender is a member of marketing" -ExceptIfFrom "Kelly Rollin"
 #arguments  "ui" 
 #download current set
 #check for dups
