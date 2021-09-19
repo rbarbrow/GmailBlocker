42157b2adf3d14aab29a193b6126b1005e3481ff
rbarbrow <rbarbrow@markonsolutions.com>
2021-09-19 13:23:21 -0400

    working

--- GmailBlockerAddException.ps1
+++ GmailBlockerAddException.ps1
@@ -3,22 +3,40 @@
 Import-Module ExchangeOnlineManagement
 Connect-ExchangeOnline
 
-# -1 is unlimited
-#$formatenumerationlimit = -1
 
-Get-TransportRule
-Get-TransportRule "Gmail Block" | Select-Object * | Format-List
+
+#Get-TransportRule
+#Get-TransportRule "Gmail Block" | Select-Object * | Format-List
 #Get-TransportRule "Gmail Block" | Select-Object "ExceptIfFrom" | Format-List  | Out-File "c:\z\gmailrule.txt"
-$test = Get-TransportRule "Gmail Block" | Select-Object "ExceptIfFrom" | Format-List  
+#$test = Get-TransportRule "Gmail Block" | Select-Object -expandproperty "ExceptIfFrom" | Format-List  
+$safesenderlist = Get-TransportRule "Gmail Block test" | Select-Object -expandproperty "ExceptIfFrom" 
+$safesenderlist.length
+$NewEmail = Read-Host -Prompt 'Input the Gmail address to add'
+$safesenderindex = $safesenderlist.length+1
+$safesenderlist[$safesenderindex] = $NewEmail
+Write-Output $safesenderlist
+Set-TransportRule -Identity "Gmail Block test" -ExceptIfFrom $safesenderlist
 
-Write-Output $test
-Get-TransportRule "Gmail Block" | Select-Object "ExceptIfFrom" 
-#Set-TransportRule -Identity "Sender is a member of marketing" -ExceptIfFrom "Kelly Rollin"
+
+#Get-TransportRule "Gmail Block" | Select-Object "ExceptIfFrom" 
+#Set-TransportRule -Identity "Gmail Block test" -ExceptIfFrom "rbarbrow@gmail.com"
+
+
+##check safesenderlist for dups
+##add new email to array
+
+
+
 #arguments  "ui" 
 #download current set
 #check for dups
 #add to list
 #upload list
+
+# must be an array to add
+# $blockertest = @('rbarbrow@gmail.com','lbarbrow@gmail.com')
+# Set-TransportRule -Identity "Gmail Block test" -ExceptIfFrom $blockertest
+
 
 
 #superstart goals
