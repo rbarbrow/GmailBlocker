91ece815f654d8a0d8f42a2cf2131c6df51ffe35
rbarbrow <rbarbrow@markonsolutions.com>
2021-09-20 20:35:43 -0400

    Production run 1

--- GmailBlockerAddException.ps1
+++ GmailBlockerAddException.ps1
@@ -1,45 +1,27 @@
 #GmailBlocker
+
+
 #Install-Module -Name ExchangeOnlineManagement
 Import-Module ExchangeOnlineManagement
 Connect-ExchangeOnline
 
 
+#get exception list
+$safesenderlist = Get-TransportRule "Gmail Block" | Select-Object -expandproperty "ExceptIfFrom" 
+#$safesenderlist.length
 
-#Get-TransportRule
-#Get-TransportRule "Gmail Block" | Select-Object * | Format-List
-#Get-TransportRule "Gmail Block" | Select-Object "ExceptIfFrom" | Format-List  | Out-File "c:\z\gmailrule.txt"
-#$test = Get-TransportRule "Gmail Block" | Select-Object -expandproperty "ExceptIfFrom" | Format-List  
-$safesenderlist = Get-TransportRule "Gmail Block test" | Select-Object -expandproperty "ExceptIfFrom" 
-$safesenderlist.length
+#ask for email
 $NewEmail = Read-Host -Prompt 'Input the Gmail address to add'
-$safesenderindex = $safesenderlist.length+1
-$safesenderlist[$safesenderindex] = $NewEmail
-Write-Output $safesenderlist
-Set-TransportRule -Identity "Gmail Block test" -ExceptIfFrom $safesenderlist
+if ($safesenderlist.Contains($NewEmail)) {
+    
+    Write-Host "Email is already in the list"
+}
+else {
+    
+    $safesenderlist += $NewEmail
+    Write-Output $safesenderlist
+    Set-TransportRule -Identity "Gmail Block test" -ExceptIfFrom $safesenderlist
+}
 
-
-#Get-TransportRule "Gmail Block" | Select-Object "ExceptIfFrom" 
-#Set-TransportRule -Identity "Gmail Block test" -ExceptIfFrom "rbarbrow@gmail.com"
-
-
-##check safesenderlist for dups
-##add new email to array
-
-
-
-#arguments  "ui" 
-#download current set
-#check for dups
-#add to list
-#upload list
-
-# must be an array to add
-# $blockertest = @('rbarbrow@gmail.com','lbarbrow@gmail.com')
-# Set-TransportRule -Identity "Gmail Block test" -ExceptIfFrom $blockertest
-
-
-
-#superstart goals
-#audit 
-#accept csv file 
-#scan mail trace to get csv file to feed in
+write-output "Email address added"
+Read-Host -Prompt 'press any key to close'
